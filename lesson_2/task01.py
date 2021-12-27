9dc1aa38e16e1b1a495e493a0b5f64c61cc931c8
Andrei-BM <96584756+Andrei-BM@users.noreply.github.com>
2021-12-27 13:35:43 +0300

    Delete lesson_2 directory

--- lesson_2/task01.py
+++
@@ -1,7 +0,0 @@
-# Проверка классов элементов, содержащихся в строке.
-list_01 = [10, 'word', False, 2.789, None, 'Alex', -15, 0b100, 0o37, 0x1F, True, complex(7, 9), [1, 2, 3, 'start'], {1:'Yes', 2:'No'}, ('one', 'two', 'three'), {'tangerin', 'kiwi', 'mango'}]
-
-for n, item in enumerate(list_01, 1):
-    print(f'{n}. {item:} - {type(item)}')
-# Конец программы.
-
